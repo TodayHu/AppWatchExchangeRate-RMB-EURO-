bfa0d5b6190a7a41e2153173ba66196dfd573e81
张昊 <119352@supinfo.com>
2014-12-01 21:09:24 +0100

    add watch Extensions and configure the model

---
+++ BitWatch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,35 @@
+//
+//  InterfaceController.swift
+//  BitWatch WatchKit Extension
+//
+//  Created by 张昊 on 14/12/1.
+//  Copyright (c) 2014年 Razeware LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override init(context: AnyObject?) {
+        // Initialize variables here.
+        super.init(context: context)
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+}
---
+++ BitWatch WatchKit Extension/NotificationController.swift
@@ -0,0 +1,56 @@
+//
+//  NotificationController.swift
+//  BitWatch WatchKit Extension
+//
+//  Created by 张昊 on 14/12/1.
+//  Copyright (c) 2014年 Razeware LLC. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+        NSLog("%@ init", self)
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+        NSLog("%@ will activate", self)
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        NSLog("%@ did deactivate", self)
+        super.didDeactivate()
+    }
+
+    /*
+    override func didReceiveLocalNotification(localNotification: UILocalNotification, withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        // This method is called when a local notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification inteface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.Custom)
+    }
+    */
+    
+    /*
+    override func didReceiveRemoteNotification(remoteNotification: [NSObject : AnyObject], withCompletion completionHandler: ((WKUserNotificationInterfaceType) -> Void)) {
+        // This method is called when a remote notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification inteface as quickly as possible.
+        //
+        // After populating your dynamic notification interface call the completion block.
+        completionHandler(.Custom)
+    }
+    */
+}
