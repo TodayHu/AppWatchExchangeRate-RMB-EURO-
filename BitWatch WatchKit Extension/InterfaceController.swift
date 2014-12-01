edd49d33be6222e70355e6aaba3d3174d17c5cfa
张昊 <119352@supinfo.com>
2014-12-01 22:07:47 +0100

    Finish the function to display the rate in apple watch

--- BitWatch WatchKit Extension/InterfaceController.swift
+++ BitWatch WatchKit Extension/InterfaceController.swift
@@ -8,13 +8,131 @@
 
 import WatchKit
 import Foundation
-
+import BitWatchKit
 
 class InterfaceController: WKInterfaceController {
 
+     var trc=Tracker()
+    
+    
+    
+    
+    
+    @IBOutlet weak var PriceLabel: WKInterfaceLabel!
+    
+    
+    @IBOutlet weak var image: WKInterfaceImage!
+    
+    @IBOutlet weak var updateTime: WKInterfaceLabel!
+    
+    
+    
+
+    
+    @IBAction func Refresh() {
+        
+        
+        updateTimeZone(trc.cachedDate())
+        updatePrice()
+        println( "time is :\(NSDate())")
+    }
+    
+    
+    
+    
+    func updatePrice() {
+        
+        
+        
+        let oldPrice=trc.cachedPrice()
+        
+        trc.requestPrice { (price, error) -> () in
+            
+            if let err = error
+            
+            
+            {
+                println( "error is :\(err)")  }
+            
+            else{
+                
+                self.PriceLabel.setText(Tracker.priceFormatter.stringFromNumber(price!.doubleValue))
+                
+                self.updateImg(oldPrice, newPrice: price!)
+            
+                self.updateTimeZone(NSDate())
+            }
+         
+            
+            
+            
+            
+            
+        }
+        
+    }
+
+    func  updateImg(oldPrice:NSNumber,newPrice:NSNumber) {
+        
+        
+        if newPrice.isEqualToNumber(oldPrice){
+        
+        
+        image.setHidden(true)
+            
+            
+            
+        
+        }else {
+        
+            if newPrice.doubleValue > oldPrice.doubleValue{image.setImageNamed("Up")}
+            else{image.setImageNamed("Down")}
+        
+        
+        image.setHidden(false)
+        
+        
+        
+        }
+        
+
+        
+    }
+
+    func updateTimeZone(date:NSDate) {
+        
+   
+        updateTime.setText(Tracker.dateFormatter.stringFromDate(date))
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override init(context: AnyObject?) {
         // Initialize variables here.
         super.init(context: context)
+        
+        Refresh()
         
         // Configure interface objects here.
         NSLog("%@ init", self)
